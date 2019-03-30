4b610eef3e135ff7ad1630a314deee069c3f0c05
ssk <ss.kovalevskiy@yandex.ru>
2019-03-30 14:04:54 +0300

    Создал маршруты для компонентов Conference и NotFound

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ConferenceModule} from './conference/conference.module';
+import {ConferenceComponent} from './conference/conference.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ConferenceComponent},
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), ConferenceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
