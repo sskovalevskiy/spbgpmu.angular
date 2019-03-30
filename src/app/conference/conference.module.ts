3e0d03be3bad522b90c2a8f8d08e75dac3f6fe2c
ssk <ss.kovalevskiy@yandex.ru>
2019-03-30 14:01:24 +0300

    Создал компонент и модуль Conference

---
+++ src/app/conference/conference.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ConferenceRoutingModule } from './conference-routing.module';
+import { ConferenceComponent } from './conference.component';
+
+@NgModule({
+  declarations: [ConferenceComponent],
+  imports: [
+    CommonModule,
+    ConferenceRoutingModule
+  ],
+  exports: []
+})
+export class ConferenceModule { }
