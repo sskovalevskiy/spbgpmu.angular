3115e4d596c506fb362c023228193f4f04891f05
ssk <ss.kovalevskiy@yandex.ru>
2019-08-02 01:26:23 +0300

    Добавил HeaderComponent и стили для book-store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { FooterComponent } from './footer/footer.component';
 import { BookStoreComponent } from './book-store/book-store.component';
+import { HeaderComponent } from './book-store/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotFoundComponent,
     FooterComponent,
-    BookStoreComponent
+    BookStoreComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
--- src/app/book-store/book-store.component.ts
+++ src/app/book-store/book-store.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-book-store',
   templateUrl: './book-store.component.html',
-  styleUrls: ['./book-store.component.css']
+  styleUrls: ['./book-store.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class BookStoreComponent implements OnInit {
 
