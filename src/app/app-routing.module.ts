d5553b48850bb0d0182cb03bff4c124a4c3ac9c1
sskovalevskiy <ss.kovalevskiy@andex.ru>
2020-04-07 23:06:14 +0300

    Добавил компонент News в корневую маршрутизацию

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import {NotFoundComponent} from './not-found/not-found.component';
 import {CongressComponent} from './congress/congress.component';
 import {CongressModule} from './congress/congress.module';
+import {NewsComponent} from './news/news.component';
 
 const routes: Routes = [
   { path: '', component: CongressComponent},
+  { path: 'news', component: NewsComponent},
   { path: '**', component: NotFoundComponent }
 ];
 
