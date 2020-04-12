12199c8a931abe76a820efb7c73dbad435cf3cf6
sskovalevskiy <ss.kovalevskiy@andex.ru>
2020-04-12 03:43:35 +0300

    Создал компонент Article

--- src/app/news/news.module.ts
+++ src/app/news/news.module.ts
@@ -1,12 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NewsListComponent } from './news-list/news-list.component';
-import { LatestPostComponent } from './latest-post/latest-post.component';
-import { PaginationComponent } from './pagination/pagination.component';
-import { NewsCarouselComponent } from './news-carousel/news-carousel.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {NewsListComponent} from './news-list/news-list.component';
+import {LatestPostComponent} from './latest-post/latest-post.component';
+import {PaginationComponent} from './pagination/pagination.component';
+import {NewsCarouselComponent} from './news-carousel/news-carousel.component';
+import {ArticleComponent} from './article/article.component';
+import {NewsRoutingModule} from './news-routing.module';
 
 @NgModule({
-  declarations: [NewsListComponent, LatestPostComponent, PaginationComponent, NewsCarouselComponent],
+  declarations: [
+    NewsListComponent,
+    LatestPostComponent,
+    PaginationComponent,
+    NewsCarouselComponent,
+    ArticleComponent
+  ],
   exports: [
     NewsListComponent,
     LatestPostComponent,
@@ -14,7 +22,9 @@ import { NewsCarouselComponent } from './news-carousel/news-carousel.component';
     NewsCarouselComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    NewsRoutingModule
   ]
 })
-export class NewsModule { }
+export class NewsModule {
+}
