226ea8a4dc3fefc0eb7d04efbde49378a711a3a8
sskovalevskiy <ss.kovalevskiy@andex.ru>
2020-04-17 00:56:56 +0300

    Добавил в модель Category поле description

--- src/app/news/models/category.model.ts
+++ src/app/news/models/category.model.ts
@@ -1,6 +1,7 @@
 export class Category {
   constructor(public id: number,
               public title: string,
+              public description: string,
               public url: string
   ) {
   }
