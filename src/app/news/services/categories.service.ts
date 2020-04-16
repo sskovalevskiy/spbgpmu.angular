35032c9bc7c139252393d3a434784c46287eacc8
sskovalevskiy <ss.kovalevskiy@andex.ru>
2020-04-17 01:23:20 +0300

    Добавил значения categories в сервис

--- src/app/news/services/categories.service.ts
+++ src/app/news/services/categories.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Category} from '../models/category.model';
+import {Observable, of} from 'rxjs';
+
+const categories: Array<Category> = [
+  new Category(1, 'History', 'Posts full of historic value and useful information.', 'history'),
+  new Category(2, 'Science', 'Posts for the most curious ones..', 'science'),
+  new Category(3, 'Sports', 'All about that game last night.', 'sports'),
+  new Category(4, 'Travel', 'Everything you need to know for your next trip.', 'travel'),
+  new Category(5, 'Getting Started', 'Learn to use Ghost.', 'getting-started')
+];
+
+const categoriesObservable: Observable<Array<Category>> = of(categories);
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
 
-  constructor() { }
+  constructor() {
+  }
+
+  getCategories(): Observable<Category[]> {
+    return categoriesObservable;
+  }
 }
