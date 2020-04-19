ec68de2614f00844afab2129292e004b45185136
sskovalevskiy <ss.kovalevskiy@yandex.ru>
2020-04-20 00:16:15 +0300

    Изменил типы полей модели Article

--- src/app/news/models/article.model.ts
+++ src/app/news/models/article.model.ts
@@ -1,16 +1,12 @@
-import {Category} from './category.model';
-import {Author} from './author.model';
-
 export class Article {
 
   constructor(public id: number,
               public headline: string,
-              public articleLink: string,
               public articleBackgroundImageLink: string,
-              public datePublished: string,
-              public dateModified: string,
+              public datePublished: number,
+              public dateModified: number,
               public categoryId: number,
-              public author: Author,
+              public authorId: number,
               public articleText?: string
               ) {
   }
