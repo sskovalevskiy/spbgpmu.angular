b2ed1aaaf914a969a7d6e4375323d0ff9fc1a4b4
sskovalevskiy <ss.kovalevskiy@yandex.ru>
2020-04-19 17:12:48 +0300

    Добавил в NewsListComponent получение по id объекта Category из CategoriesService

--- src/app/news/news-list/news-list.component.ts
+++ src/app/news/news-list/news-list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 import {Article} from '../models/article.model';
 import {NewsService} from '../services/news.service';
+import {CategoriesService} from '../services/categories.service';
 
 @Component({
   selector: 'app-news-list',
@@ -12,10 +13,15 @@ export class NewsListComponent implements OnInit {
 
   news$: Observable<Array<Article>>;
 
-  constructor(private newsService: NewsService) {
+  constructor(private newsService: NewsService,
+              private categoriesService: CategoriesService) {
   }
 
   ngOnInit() {
     this.news$ = this.newsService.getNews();
+  }
+
+  getCategoryById(categoryId: number) {
+    return this.categoriesService.getCategoryById(categoryId);
   }
 }
--- src/app/news/services/categories.service.ts
+++ src/app/news/services/categories.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
+import {Category} from '../models/category.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,10 @@ export class CategoriesService {
 
   getCategories(): Observable<any> {
     return this.http.get('./assets/categories.json');
+  }
+
+  getCategoryById(categoryId: number) {
+    return new Category(1, 'Science', 'Posts for the most curious ones..', 'science',
+      'http://157.230.217.118:3001/content/images/2019/07/photo-1559762691-617a33825bc6.jpeg');
   }
 }
