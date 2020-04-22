eaa3ca7a286d3fd0906149da4c7d9ac0e29db4b6
sskovalevskiy <ss.kovalevskiy@yandex.ru>
2020-04-22 23:58:42 +0300

    Настроил маршрутизацию к компоненту AuthorComponent

--- src/app/news/news-routing.module.ts
+++ src/app/news/news-routing.module.ts
@@ -4,19 +4,21 @@ import {NewsListComponent} from './news-list/news-list.component';
 import {CategoriesComponent} from './categories/categories.component';
 import {ArticleComponent} from './article/article.component';
 import {AuthorsComponent} from './authors/authors.component';
+import {AuthorComponent} from './author/author.component';
 
 const routes: Routes = [
   {
     path: 'news', children: [
-      { path: '', component: NewsListComponent },
-      { path: ':id', component: ArticleComponent }
+      {path: '', component: NewsListComponent},
+      {path: ':id', component: ArticleComponent}
     ]
   },
-  { path: 'categories', component: CategoriesComponent },
-  { path: 'tag/:category', component: CategoriesComponent },
-  { path: 'authors', children: [
-      { path: '', component: AuthorsComponent },
-      { path: ':name', redirectTo: '/authors' }
+  {path: 'categories', component: CategoriesComponent},
+  {path: 'tag/:category', component: CategoriesComponent},
+  {
+    path: 'authors', children: [
+      {path: '', component: AuthorsComponent},
+      {path: ':name', component: AuthorComponent}
     ]
   }
 ];
