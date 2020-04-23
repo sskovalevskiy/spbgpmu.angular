e9fc508d3bdd7ec5c46cf5ee2b2f01c69bd6a6ea
sskovalevskiy <ss.kovalevskiy@yandex.ru>
2020-04-23 16:50:25 +0300

    Заменил в news-routing компонент, отвечающий за отображение /news

--- src/app/news/news-routing.module.ts
+++ src/app/news/news-routing.module.ts
@@ -1,22 +1,23 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {NewsListComponent} from './news-list/news-list.component';
 import {CategoriesComponent} from './categories/categories.component';
 import {ArticleComponent} from './article/article.component';
 import {AuthorsComponent} from './authors/authors.component';
+import {NewsComponent} from './news.component';
 
 const routes: Routes = [
   {
     path: 'news', children: [
-      { path: '', component: NewsListComponent },
-      { path: ':id', component: ArticleComponent }
+      {path: '', component: NewsComponent},
+      {path: ':id', component: ArticleComponent}
     ]
   },
-  { path: 'categories', component: CategoriesComponent },
-  { path: 'tag/:category', component: CategoriesComponent },
-  { path: 'authors', children: [
-      { path: '', component: AuthorsComponent },
-      { path: ':name', redirectTo: '/authors' }
+  {path: 'categories', component: CategoriesComponent},
+  {path: 'tag/:category', component: CategoriesComponent},
+  {
+    path: 'authors', children: [
+      {path: '', component: AuthorsComponent},
+      {path: ':name', redirectTo: '/authors'}
     ]
   }
 ];
