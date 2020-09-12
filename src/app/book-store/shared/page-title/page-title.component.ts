73e8bebc6f9747e4f6f254daa0f37524cd89f3d5
ssk <ss.kovalevskiy@yandex.ru>
2020-09-13 01:08:17 +0300

    Рефакторинг: заменил html-разметку компонентов на единый shared PageTitleComponent

--- src/app/book-store/shared/page-title/page-title.component.ts
+++ src/app/book-store/shared/page-title/page-title.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-page-title',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageTitleComponent implements OnInit {
 
-  pageTitle: string;
+  @Input() pageTitle: string;
 
   constructor() { }
 
