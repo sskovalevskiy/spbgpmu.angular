7d420ee09b9538dded0cfb570092f2a4e90843c4
ssk <ss.kovalevskiy@yandex.ru>
2020-09-13 23:51:24 +0300

    Рефакторинг: почистил html-разметку ProductsComponent - удалил повторяющиеся книги

--- src/app/book-store/shared/sidebar/products/products.component.ts
+++ src/app/book-store/shared/sidebar/products/products.component.ts
@@ -9,6 +9,8 @@ export class ProductsComponent implements OnInit {
 
   @Input() title: string;
   @Input() searchType: string; //Тип поиска: 'Bestsellers', 'Top Rated', 'Sale', 'Random Book', 'Random Journal',
+  //TODO сделать передачу параметра с требуемым количеством книг (в widget - 3, footer - 4, sidebar - 5)
+  // @Input() booksAmount: number;
 
   constructor() { }
 
