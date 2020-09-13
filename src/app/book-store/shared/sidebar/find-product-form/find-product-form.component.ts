e77379d8d09cab93666e816e170537e3d7c41055
ssk <ss.kovalevskiy@yandex.ru>
2020-09-14 01:12:17 +0300

    Рефакторинг: почистил html-разметку FindProductFormComponent, добавил поля, сделал необязательным @Input():title

--- src/app/book-store/shared/sidebar/find-product-form/find-product-form.component.ts
+++ src/app/book-store/shared/sidebar/find-product-form/find-product-form.component.ts
@@ -7,8 +7,15 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class FindProductFormComponent implements OnInit {
 
-  @Input() title: string[];
-  constructor() { }
+  @Input() title?: string[] = ["Find","Product"];
+
+  categories: string[] = ["Programming","Javascript","Web Application", "HTML5", "SEO", "Web Design"];
+
+  authors: string[] = ["Thoriq Firdaus", "Tim Kadlec", "Steve Krug", "Susan Weinschenk", "Austin Kleon", "Ellen Lupton",
+    "Patrick McNeil", "Brian Miller", "Kyle Simpson","Nicholas C. Zakas"]
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
