37e62cc61031319d0e64ede303c96a19d845655b
ssk <ss.kovalevskiy@yandex.ru>
2020-09-18 11:36:29 +0300

    Реализовал использование сервиса CategoryService в компоненте ProductCategoriesComponent

--- src/app/book-store/shared/sidebar/product-categories/product-categories.component.ts
+++ src/app/book-store/shared/sidebar/product-categories/product-categories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Category} from "../../../services/models/category.model";
+import {CategoryService} from "../../../services/category.service";
 
 @Component({
   selector: 'app-product-categories',
@@ -7,11 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCategoriesComponent implements OnInit {
 
-  categories: string[] = ["Programming","Javascript","Web Application", "HTML5", "SEO", "Web Design"];
+  categories: Array<Category>;
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit() {
+    this.getCategories().catch(err => console.log(err));
+  }
+
+  private async getCategories(){
+    this.categories = await this.categoryService.getCategories();
   }
 
 }
